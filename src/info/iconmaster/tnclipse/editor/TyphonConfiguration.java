78cf01e958511509917abe7aff1bd9b1c23fb2f5
Iconmaster <crazybonesj@yahoo.com>
2017-06-28 15:00:22 -0400

    Fixed hovers for errors

--- src/info/iconmaster/tnclipse/editor/TyphonConfiguration.java
+++ src/info/iconmaster/tnclipse/editor/TyphonConfiguration.java
@@ -1,17 +1,19 @@
 package info.iconmaster.tnclipse.editor;
 
+import org.eclipse.jface.text.DefaultTextHover;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextHover;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
+import org.eclipse.jface.text.source.DefaultAnnotationHover;
+import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
-import info.iconmaster.tnclipse.ColorManager;
-
 public class TyphonConfiguration extends SourceViewerConfiguration {
 	TyphonTokenScanner scanner;
-	
+
 	public TyphonConfiguration() {
 		scanner = new TyphonTokenScanner();
 	}
@@ -19,11 +21,21 @@ public class TyphonConfiguration extends SourceViewerConfiguration {
 	@Override
 	public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
 		PresentationReconciler reconciler = new PresentationReconciler();
-		
+
 		DefaultDamagerRepairer dr = new DefaultDamagerRepairer(scanner);
 		reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
 		reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
-		
+
 		return reconciler;
+	}
+
+	@Override
+	public ITextHover getTextHover(ISourceViewer sourceViewer, String contentType) {
+		return new DefaultTextHover(sourceViewer);
+	}
+	
+	@Override
+	public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer) {
+		return new DefaultAnnotationHover();
 	}
 }
